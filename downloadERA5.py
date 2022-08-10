add12cc2c724ededd41f45cecd1f6f8cafb5b63f
Gaige Hunter Kerr <gaige.h.kerr@gmail.com>
2022-08-10 08:25:59 -0400

    Clean for publication

--- downloadERA5.py
+++ downloadERA5.py
@@ -28,16 +28,16 @@ def download_era5(year):
     c = cdsapi.Client()
     # Variable names in CDS can be found at 
     # https://confluence.ecmwf.int/display/CKB/ERA5%3A+data+documentation
-    VARS = ['surface_solar_radiation_downwards', 
-         'surface_pressure', 'potential_evaporation', '2m_temperature', 
+    VARS = ['surface_pressure','surface_solar_radiation_downwards', 
+         'potential_evaporation', '2m_temperature', 
          'surface_latent_heat_flux', '2m_dewpoint_temperature', 
          'volumetric_soil_water_layer_1', 'volumetric_soil_water_layer_2',
          'volumetric_soil_water_layer_3', 'volumetric_soil_water_layer_4',
-         '10m_v_component_of_wind', '10m_u_component_of_wind', 'total_precipitation']    
+         '10m_v_component_of_wind', '10m_u_component_of_wind', 'total_precipitation']
     YEARS = [x for x in map(str, range(year, year+1))]
     for V in VARS:
         for Y in YEARS:
-            target_filename = 'era5-hourly-'+V+'-'+Y+'a.nc'
+            target_filename = 'era5-hourly-'+V+'-'+Y+'.nc'
             if not os.path.isfile(target_filename):
      	        c.retrieve(
      	            'reanalysis-era5-single-levels', {
@@ -47,18 +47,17 @@ def download_era5(year):
                      # If months in the latter part of the year are selected
                      # the target_filename SHOULD NOT end with "a". The "a" 
                      # suffix denotes the beginning of the year. 
-                     #'month':['09','10','11','12'],
-                     'month':['01','02','03','04','05','06','07','08'],
+                'month':['01','02','03','04','05','06','07','08','09','10',
+                    '11','12'],
      		     'day':['01','02','03','04','05','06','07','08','09',
-     		            '10','11','12','13','14','15','16','17','18','19',
-                         '20','21','22','23','24','25','26','27','28','29',
-                         '30','31'],
-                     'time':['00:00','01:00','02:00','03:00','04:00','05:00',
- 	                    '06:00','07:00','08:00','09:00','10:00','11:00',
-                         '12:00','13:00','14:00','15:00','16:00','17:00',
-                         '18:00','19:00','20:00','21:00','22:00','23:00'],
-                     'format':'netcdf'
-                     },target_filename)
+                     '10','11','12','13','14','15','16','17','18','19',
+                     '20','21','22','23','24','25','26','27','28','29',
+                     '30','31'],
+                'time':['00:00','01:00','02:00','03:00','04:00','05:00',
+                    '06:00','07:00','08:00','09:00','10:00','11:00',
+                    '12:00','13:00','14:00','15:00','16:00','17:00',
+                    '18:00','19:00','20:00','21:00','22:00','23:00'],
+                'format':'netcdf'},target_filename)
     # Repeat the above but for variables available on pressure levels rather 
     # than on single levels; see 
     # https://confluence.ecmwf.int/display/WEBAPI/ERA5+monthly+means+of+daily+means+retrieval+efficiency
@@ -100,7 +99,6 @@ def download_era5(year):
     return
 
 import numpy as np
-for year in np.arange(2020, 2021, 1):
+for year in np.arange(2022, 2023):
     print(year)
     download_era5(year)
-
